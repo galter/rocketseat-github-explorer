3b0e8f8a83030b9c9f189dce1b2ff1c5af9bfce3
galter <ehgalter@gmail.com>
2021-03-06 21:09:56 -0300

    Create Repository Component and Style

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,70 @@
 import React from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Link, useRouteMatch } from 'react-router-dom';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer"/>
+
+        <Link to="/dashaboard">
+          <FiChevronLeft size={16}/>
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img 
+            src="https://avatars.githubusercontent.com/u/3959396?s=460&u=58f315f83bb7a66d71430f377dee0c1fc990ff39&v=4" 
+            alt=""
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Descrição do repositorio</p>
+          </div>
+        </header>
+        
+        <ul>
+          <li>
+            <strong>1890</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link 
+          to="asdsds"
+        >
+          <div>
+            <strong>ajdjakdhasjdhskadjhskjd</strong>
+            <p>dsasfjsdfhjkdshfkjdshkjdfh</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  )
 };
 
 export default Repository;

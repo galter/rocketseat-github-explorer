0df782b44674f58a3616d8996149619f8c9ee6d9
galter <ehgalter@gmail.com>
2021-03-06 21:56:31 -0300

    Connect Repository Component with API to Show info and Issues

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import { Link, useRouteMatch } from 'react-router-dom';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -9,59 +10,124 @@ interface RepositoryParams {
   repository: string;
 }
 
+interface RepositoryI {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
+interface Issue {
+  id: number;
+  title: string,
+  html_url: string;
+  user: {
+    login: string;
+  }
+}
+
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<RepositoryI | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
   const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    api.get(`/repos/${params.repository}`).then(response => {
+      setRepository(response.data);
+    });
+    api.get(`/repos/${params.repository}/issues`).then(response => {
+      setIssues(response.data);
+
+    });
+
+    // This way the second req wait first finish... 
+    /* async function loadData(): Promise<boid> {
+      const repositories = await api.get(`/repos/${params.repository}`);
+      const issues = await api.get(`/repos/${params.repository}/issues`);
+
+      console.log(repositories);
+      console.log(issues);
+    }
+
+    loadData(); */
+ 
+    // This rack executes both in same time... 
+    /* async function loadData(): Promise<boid> {
+      const [repositories, issues] = await Promise.all([ //Promise.race >> return first that execute...
+        api.get(`/repos/${params.repository}`),
+        api.get(`/repos/${params.repository}/issues`)
+      ]);
+
+      console.log(repositories);
+      console.log(issues);
+    }
+
+    loadData(); */
+  }, [params.repository]);
 
   return (
     <>
       <Header>
         <img src={logoImg} alt="Github Explorer"/>
 
-        <Link to="/dashaboard">
+        <Link to="/">
           <FiChevronLeft size={16}/>
           Voltar
         </Link>
       </Header>
 
-      <RepositoryInfo>
-        <header>
-          <img 
-            src="https://avatars.githubusercontent.com/u/3959396?s=460&u=58f315f83bb7a66d71430f377dee0c1fc990ff39&v=4" 
-            alt=""
-          />
-          <div>
-            <strong>Rocketseat/unform</strong>
-            <p>Descrição do repositorio</p>
-          </div>
-        </header>
-        
-        <ul>
-          <li>
-            <strong>1890</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>48</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>67</strong>
-            <span>Issues abertas</span>
-          </li>
-        </ul>
-      </RepositoryInfo>
+      { repository ? (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      ) : (
+        <p>Carregando...</p>
+      )}
 
       <Issues>
-        <Link 
-          to="asdsds"
-        >
-          <div>
-            <strong>ajdjakdhasjdhskadjhskjd</strong>
-            <p>dsasfjsdfhjkdshfkjdshkjdfh</p>
-          </div>
+        {issues.map(issue => (
+          <a
+            key={issue.id}
+            href={issue.html_url}
+          >
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </Link>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Issues>
     </>
   )
